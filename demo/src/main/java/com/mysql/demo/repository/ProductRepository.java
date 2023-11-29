7e38cc0af2b08009b8928ee0c6d1f58c9917bae7
RupeshRajKK <129301647+RupeshRajKK@users.noreply.github.com>
2023-11-29 18:09:17 +0530

    Add files via upload

---
+++ demo/src/main/java/com/mysql/demo/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.mysql.demo.repository;
+
+import com.mysql.demo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByName(String name);
+}
+
